dc02950f3530608c33ba3d81ca899b702d8572ba
Rafael Suzano <rafaelsuzano@hotmail.com>
2019-11-18 00:43:21 -0300

    ajuste linux

--- automacao.py
+++ automacao.py
@@ -1,16 +1,29 @@
 #D:\automacao\drivers\chromedriver_win32
-
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 
-driver = webdriver.Chrome('D:/automacao/drivers/chromedriver_win32/chromedriver.exe')
-#driver = webdriver.Chrome('/opt/suzanoit/automacao/drivers/chrome/chromedriver',0755)
+
+#driver = webdriver.Chrome('D:/automacao/drivers/chromedriver_win32/chromedriver.exe')
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 
-driver.maximize_window()
+
+
+
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",chrome_options=chrome_options)
+                              
+
+#driver.maximize_window()
+driver.set_window_size(1920, 1080)
 #driver.fullscreen_window()
 driver.get('https://www.uol.com.br')
-driver.save_screenshot('evidência.png')
+driver.save_screenshot('evidencia.png')
 
 driver.quit()
